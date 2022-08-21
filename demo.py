97006141961b4f0a2dce45ec562209e64f4a95e3
Rahul Mac <raj143125@gmail.com>
2022-08-21 16:03:58 +0530

    Files added

---
+++ demo.py
@@ -0,0 +1,56 @@
+"""
+This file is part of Review Sentiment Analyser.
+
+Review Sentiment Analyser is free software: you can redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+Review Sentiment Analyser is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Review Sentiment Analyser. If not, see
+<https://www.gnu.org/licenses/>.
+"""
+
+import pandas as pd
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM,Dense, Dropout, SpatialDropout1D
+from tensorflow.keras.layers import Embedding
+from keras.models import load_model
+
+def predict_sentiment(text):
+    t = token.texts_to_sequences([text])
+    t = pad_sequences(t, maxlen = 200)
+    pred = int(model.predict(t).round().item())
+    print("Predicted label: ", label[1][pred])
+
+df = pd.read_csv("data.csv")
+label = df.sentiment.factorize()
+text = df.review.values
+token = Tokenizer(num_words = 5000)
+token.fit_on_texts(text)
+vocab_size = len(token.word_index) + 1
+encoded_docs = token.texts_to_sequences(text)
+pad_seq = pad_sequences(encoded_docs, maxlen = 200)
+embedding_vector_length = 32
+
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_vector_length, input_length = 200))
+model.add(SpatialDropout1D(0.25))
+model.add(LSTM(50, dropout = 0.5, recurrent_dropout = 0.5))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation = 'sigmoid'))
+model.compile(loss = 'binary_crossentropy',optimizer = 'adam', metrics = ['accuracy'])
+print(model.summary())
+model.fit(pad_seq, label[0], validation_split = 0.2, epochs = 1, batch_size = 32)
+
+model.save('model.h5')
+del model
+model = load_model('model.h5')
+
+while(True):
+    sample = input("Write a review: ")
+    predict_sentiment(sample)
---
+++ main.py
@@ -0,0 +1,81 @@
+"""
+This file is part of Review Sentiment Analyser.
+
+Review Sentiment Analyser is free software: you can redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+Review Sentiment Analyser is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Review Sentiment Analyser. If not, see
+<https://www.gnu.org/licenses/>.
+"""
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import pandas as pd
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+
+class Window:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Review Sentiment Analyser")
+        self.window.resizable(0,0)
+        self.window.geometry("780x580")
+        self.window.iconbitmap("icon.ico")
+        self.widgets()
+        self.flag = 0
+        self.window.mainloop()
+
+    def widgets(self):
+        self.frame = ttk.LabelFrame(self.window, text = "Review")
+        self.frame.grid(column = 0, row = 0, padx = 5, pady = 5)
+
+        self.text = Text(self.frame)
+        self.text.grid(column = 0, row = 0, padx = 5, pady = 5)
+        self.text.configure(font = ("Times New Roman", 14))
+        self.scroll = ttk.Scrollbar(self.frame, orient = 'vertical', command = self.text.yview)
+        self.scroll.grid(row = 0, column = 1, sticky = NS, padx = 5, pady = 5)
+        self.text['yscrollcommand'] = self.scroll.set
+
+        self.btn = ttk.Button(self.frame, text = "Check Sentiment", command = self.check)
+        self.btn.grid(column = 0, row = 1, padx = 5, pady = 5)
+
+    def check(self):
+        text = self.text.get("1.0", 'end-1c')
+        text = " ".join(text.split())
+        if text == "" or text.isspace():
+            self.error()
+        else:
+            if self.flag == 0:
+                self.model()
+            self.prediction(text)
+
+    def message(self, x):
+        messagebox.showinfo("Message", "Predicted Sentiment: " + x.upper())
+  
+    def error(self):
+        messagebox.showerror("Message", "Review box cannot be empty")
+
+    def model(self):
+        self.df = pd.read_csv("data.csv")
+        self.label = self.df.sentiment.factorize()
+        self.review = self.df.review.values
+        self.token = Tokenizer(num_words = 5000)
+        self.token.fit_on_texts(self.review)
+        self.m = load_model('model.h5')
+        self.flag = 1
+
+    def prediction(self, text):
+        t = self.token.texts_to_sequences([text])
+        t = pad_sequences(t, maxlen = 200)
+        pred = int(self.m.predict(t).round().item())
+        self.message(self.label[1][pred])
+
+if __name__ == "__main__":
+    Window()
